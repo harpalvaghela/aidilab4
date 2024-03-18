130827597e4d5e67fbeef4a4edca1679581c559c
Harpal Vaghela <45502341+harpalvaghela@users.noreply.github.com>
2024-03-18 13:16:00 -0400

    Add files via upload

---
+++ FlaskApp.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+
+
+import numpy as np
+import pickle
+from flask import Flask, request
+from flask import Flask, request, jsonify, render_template
+
+app=Flask(__name__)
+pickle_in = open("classifier.pkl","rb")
+classifier=pickle.load(pickle_in)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    try:
+        int_features = [x for x in request.form.values()]
+        final_features = [np.array(int_features)]
+        prediction = classifier.predict(final_features)
+
+        return render_template('index.html', prediction_text='The fish belong to species {}'.format(prediction[0]))
+    except:
+        "Error"
+
+    
+
+
+if __name__=='__main__':
+    app.run()
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# aidilab4
+# Fish-market-model
+
+Heroku link : https://fish-market-ml-model-yashkumar-aa1d45c86ba9.herokuapp.com/
